7a9aec593ba8f6637621aea71584f5046d4fc5c7
Thiago Mota <th.araujomota@gmail.com>
2022-03-07 23:34:54 -0300

    feat: adds Loading component and Login logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,76 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      enterBtnDisabled: true,
+      isLoading: false,
+      redirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      name: target.value },
+    () => this.setState({ enterBtnDisabled: this.btnValidation() }));
+  }
+
+  handleClick = async () => {
+    const { name } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({ name });
+    this.setState({
+      isLoading: false,
+      redirect: true,
+    });
+  }
+
+  btnValidation = () => {
+    const { name } = this.state;
+    const THREE = 3;
+    if (name.length >= THREE) return false;
+    return true;
+  }
+
   render() {
+    const { enterBtnDisabled, isLoading, redirect } = this.state;
+    const { handleChange, handleClick } = this;
+    if (redirect) return <Redirect to="/search" />;
+
     return (
-      <div data-testid="page-login">
-        Login
-      </div>
+      isLoading ? <Loading /> : (
+        <div data-testid="page-login">
+          <form>
+            <label htmlFor="login-id">
+              Usuário:
+              <input
+                id="login-id"
+                data-testid="login-name-input"
+                placeholder="Insira seu usuário"
+                type="text"
+                onChange={ handleChange }
+              />
+            </label>
+
+            <button
+              data-testid="login-submit-button"
+              disabled={ enterBtnDisabled }
+              type="button"
+              onClick={ handleClick }
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
+      )
     );
   }
 }
