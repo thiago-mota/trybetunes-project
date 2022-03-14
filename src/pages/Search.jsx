017d8968e8715761109500cca00cfccb64103f01
Thiago Mota <th.araujomota@gmail.com>
2022-03-14 13:39:23 -0300

    feat: creates form, button and button validation

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,43 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+      searchBtnDisabled: true,
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch({ target }) {
+    this.setState({ artistName: target.value });
+    return target.value.length >= 2
+      ? this.setState({ searchBtnDisabled: false })
+      : this.setState({ searchBtnDisabled: true });
+  }
+
   render() {
+    const { searchBtnDisabled, artistName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+
+        <input
+          name="artistName"
+          value={ artistName }
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.handleSearch }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ searchBtnDisabled }
+        >
+          Procurar
+        </button>
       </div>
     );
   }
