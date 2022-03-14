5e4e160f2b92f3af281860ee0f039f8aee811435
Thiago Mota <th.araujomota@gmail.com>
2022-03-14 11:05:34 -0300

    feat: adds navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -35,6 +36,23 @@ class Header extends React.Component {
         <h2 data-testid="header-user-name">
           { name }
         </h2>
+
+        <nav>
+          <ul>
+            <li>
+              <Link data-testid="link-to-search" to="/search">
+                Pesquisar
+              </Link>
+              <Link data-testid="link-to-favorites" to="/favorites">
+                Favoritos
+              </Link>
+              <Link data-testid="link-to-profile" to="/profile">
+                Perfil
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
       </header>
     )
     );
